67620ea158f67a1193d5a6db50803e9dcf5401d2
dbswls627 <dbswls627@naver.com>
2022-01-07 21:50:40 +0900

    중복제거

--- app/src/main/java/com/team_3/accountbook/adapter2.java
+++ app/src/main/java/com/team_3/accountbook/adapter2.java
@@ -41,18 +41,21 @@ public class adapter2 extends RecyclerView.Adapter<adapter2.CumstomViewHolder>{
 
     @Override
     public void onBindViewHolder(@NonNull adapter2.CumstomViewHolder holder, int position) {
-
-        if(!arrayList.get(position).getMsgBody().equals("") && arrayList.get(position).getMsgAmount()!=-1) { //정규식에 예외처리가 되지 않으면..
-            holder.dt.setText(arrayList.get(position).getMsgDate().substring(1,15));
-        }
-
+        ArrayList<String> dateArray = new ArrayList<>(); //날짜만 들어있는 리스트
         ArrayList<item> Array = new ArrayList<>();
         for (item item : arrayList) {  //arrayList 의 날짜 값이랑 같은 값만 adapter로 넘겨주기 위함
             if(item.getMsgDate().substring(1,15).equals(arrayList.get(position).getMsgDate().substring(1,15))){
                 Array.add(item);
             }
-
+            if(!dateArray.contains(item.getMsgDate().substring(1, 15))) {//중복제거하기 위한 코드
+                dateArray.add(item.getMsgDate().substring(1, 15));
+            }
         }
+
+        if(!arrayList.get(position).getMsgBody().equals("") && arrayList.get(position).getMsgAmount()!=-1) {//정규화 예외처리가 나지않았을때
+            holder.dt.setText(dateArray.get(position));
+        }
+
 
         holder.rv.setAdapter(new adapter(Array,arrayList.get(position).getMsgDate().substring(1,15))); //년도 부터 날짜까지 매개변수로 넘김
         holder.rv.setLayoutManager(new LinearLayoutManager(context));
