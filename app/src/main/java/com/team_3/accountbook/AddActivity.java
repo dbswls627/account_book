d7d2aa4673875b66257f527b33dfe265c397c352
dbswls627 <dbswls627@naver.com>
2022-02-12 15:47:45 +0900

    edittext ontouck로 바로 반응하도록 변경 소수점 로직 제거

--- app/src/main/java/com/team_3/accountbook/AddActivity.java
+++ app/src/main/java/com/team_3/accountbook/AddActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.text.Editable;
 import android.text.InputType;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -30,6 +32,7 @@ public class AddActivity extends AppCompatActivity implements WayAndSortAdapter.
     boolean checkIncome = false, checkExpense = true;
     String action = "expense";
 
+    @SuppressLint("ClickableViewAccessibility")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +51,9 @@ public class AddActivity extends AppCompatActivity implements WayAndSortAdapter.
 
         mEditSum.addTextChangedListener(new NumberTextWatcher(mEditSum));       // 금액 입력반응
         mExpense.setSelected(true);
+
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);   //키보드 내리기 힐때 필요한 객체
+
         mWay.setInputType(InputType.TYPE_NULL);         // 클릭시 키보드 안올라오게 함.
         mSort.setInputType(InputType.TYPE_NULL);        // 클릭시 키보드 안올라오게 함.
 
@@ -59,6 +65,36 @@ public class AddActivity extends AppCompatActivity implements WayAndSortAdapter.
         mDate.setText(date);
         mBody.setText(body);
         mEditSum.setText(String.valueOf(amount));
+
+        mDate.setOnTouchListener((view, motionEvent) -> {       //터치즉시 이벤트 발생(mOnClick 시 2번 터치)
+            mRV_WayAndSort.setVisibility(View.INVISIBLE);
+            return false;
+        });
+        mWay.setOnTouchListener((view, motionEvent) -> {
+            imm.hideSoftInputFromWindow(mWay.getWindowToken(), 0);      //키보드 내리기 (다른 edittext 누른후 누른면 키보드가 뜸)
+            WayAndSortList = db.dao().getWayName();
+            mRV_WayAndSort.setAdapter(new WayAndSortAdapter(WayAndSortList, this));
+            mRV_WayAndSort.setLayoutManager(new LinearLayoutManager(this));
+            mRV_WayAndSort.setVisibility(View.VISIBLE);     //자산 리스트 보이기
+            return false;
+        });
+        mSort.setOnTouchListener((view, motionEvent) -> {
+            /*WayAndSortList = db.dao().getSortName();
+            mRV_WayAndSort.setAdapter(new WayAndSortAdapter(WayAndSortList, this));
+            mRV_WayAndSort.setLayoutManager(new LinearLayoutManager(this));*/
+            imm.hideSoftInputFromWindow(mWay.getWindowToken(), 0);      //키보드 내리기 (다른 edittext 누른후 누른면 키보드가 뜸)
+            mRV_WayAndSort.setVisibility(View.INVISIBLE);
+            return false;
+        });
+        mEditSum.setOnTouchListener((view, motionEvent) -> {
+
+            mRV_WayAndSort.setVisibility(View.INVISIBLE);
+            return false;
+        });
+        mBody.setOnTouchListener((view, motionEvent) -> {
+            mRV_WayAndSort.setVisibility(View.INVISIBLE);
+            return false;
+        });
 
     }
 
@@ -74,6 +110,7 @@ public class AddActivity extends AppCompatActivity implements WayAndSortAdapter.
 
         switch (v.getId()){
             case R.id.tv_income:
+                mRV_WayAndSort.setVisibility(View.GONE);    //자산 리스트 제거
                 if(!checkIncome){
                     checkIncome = true;
                     checkExpense = false;
@@ -81,10 +118,12 @@ public class AddActivity extends AppCompatActivity implements WayAndSortAdapter.
 
                     mIncome.setSelected(true);
                     mExpense.setSelected(false);
+
                 }
                 break;
 
             case R.id.tv_expense:
+                mRV_WayAndSort.setVisibility(View.GONE);    //자산 리스트 제거
                 if(!checkExpense){
                     checkIncome = false;
                     checkExpense = true;
@@ -95,19 +134,8 @@ public class AddActivity extends AppCompatActivity implements WayAndSortAdapter.
                 }
                 break;
 
-            case R.id.way:
-                WayAndSortList = db.dao().getWayName();
-                mRV_WayAndSort.setAdapter(new WayAndSortAdapter(WayAndSortList, this));
-                mRV_WayAndSort.setLayoutManager(new LinearLayoutManager(this));
-                break;
-
-//            case R.id.sort:
-//                WayAndSortList = db.dao().getSortName();
-//                mRV_WayAndSort.setAdapter(new WayAndSortAdapter(WayAndSortList, this));
-//                mRV_WayAndSort.setLayoutManager(new LinearLayoutManager(this));
-//                break;
-
             case R.id.save:
+
                 db.dao().insertCost(
                         mDate.getText().toString(),                         // 날짜 - 날짜선택박스 구현 필요
                         db.dao().getFk(mWay.getText().toString()),          // 수단 - 리스트 구현 필요
@@ -123,6 +151,8 @@ public class AddActivity extends AppCompatActivity implements WayAndSortAdapter.
                 startActivity(intent);
 
                 break;
+
+
         }
     }
 
@@ -134,18 +164,13 @@ public class AddActivity extends AppCompatActivity implements WayAndSortAdapter.
 
 
     class NumberTextWatcher implements TextWatcher {
-        private DecimalFormat df;          // 10진수의 값을 원하는 형식으로 변형해 주는 DecimalFormat 클래스 객체 df
         private DecimalFormat dfnd;        // ~ ~ DecimalFormat 클래스 객체 dfnd
-        private boolean hasFractionalPart;
         private EditText edit_sum;
 
-
         public NumberTextWatcher(EditText et){       // 생성자.
-            df = new DecimalFormat("#,###.##");        // "#,###.##" 형식 지정
-            df.setDecimalSeparatorAlwaysShown(true);
+
             dfnd = new DecimalFormat("#,###");         // "#,###" 형식 지정
             this.edit_sum = et;                        // 매개변수로 받은 EditText 를 edit_sum 에 저장
-            hasFractionalPart = false;                 // 플래그로 사용하는 boolean 값을 false 로.
         }
 
 
@@ -157,15 +182,12 @@ public class AddActivity extends AppCompatActivity implements WayAndSortAdapter.
                 int cp = edit_sum.getSelectionStart();  // 커서 위치값 cp (맨왼쪽부터 0ㅊ1ㅊ2ㅊ3...)
 
                 inilen = edit_sum.getText().length();   // edittext 에 입력된 문자열 길이 (',' 추가 전 길이)
-                String v = s.toString().replace(String.valueOf(df.getDecimalFormatSymbols().getGroupingSeparator()), "");   // 입력된 값 s에서 숫자만 추출
-                Number n = df.parse(v);                 // 문자열 v를 Number 형 n으로 치환 (Number: 숫자계의 Object 클래스)
+                String v = s.toString().replace(String.valueOf(dfnd.getDecimalFormatSymbols().getGroupingSeparator()), "");   // 입력된 값 s에서 숫자만 추출
+                Number n = dfnd.parse(v);                 // 문자열 v를 Number 형 n으로 치환 (Number: 숫자계의 Object 클래스)
 
-                if (hasFractionalPart) {                // 소수점 여부에 따라서 포맷을 달리함.
-                    edit_sum.setText(df.format(n));
-                }
-                else {
+
                     edit_sum.setText(dfnd.format(n));
-                }
+
 
                 endlen = edit_sum.getText().length();   // 입력된 전체 길이 (새로운 ',' 추가 길이)
                 int sel = (cp + (endlen - inilen));     // ★현재 커서 위치 (???)
@@ -189,16 +211,9 @@ public class AddActivity extends AppCompatActivity implements WayAndSortAdapter.
 
         public void beforeTextChanged(CharSequence s, int start, int count, int after) {
         }
-
-
+        
         public void onTextChanged(CharSequence s, int start, int before, int count) {
-                    // 소수점을 가지고 있다면
-            if (s.toString().contains(String.valueOf(df.getDecimalFormatSymbols().getDecimalSeparator()))) {
-                hasFractionalPart = true;
-            }
-            else {  // 소수점이 없다면
-                hasFractionalPart = false;
-            }
+
         }
     }
 
