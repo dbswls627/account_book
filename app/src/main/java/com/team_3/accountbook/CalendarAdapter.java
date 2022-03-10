23e686f704de9422ad276cca1559a747b5205a8c
dbswls627 <dbswls627@naver.com>
2022-03-10 15:36:44 +0900

    쿼리 getAmount -> getAmountOfDay , getAmountOfMonth로 구별 Home액티비티 수입 지출 금액 뜨도록 변경

--- app/src/main/java/com/team_3/accountbook/CalendarAdapter.java
+++ app/src/main/java/com/team_3/accountbook/CalendarAdapter.java
@@ -79,8 +79,8 @@ class CalendarAdapter extends RecyclerView.Adapter<CalendarAdapter.CalendarViewH
         DecimalFormat decFormat = new DecimalFormat("###,###");
 
         if(daysOfMonth.get(position).contains("!")){        // 이전월 일 때~
-            String preExpense = db.dao().getAmount(preYearMonth + " " + day1 + "일", "expense");      // 전월 해당 일 총 지출값
-            String preIncome  = db.dao().getAmount(preYearMonth + " " + day1 + "일", "income");       // 전월 해당 일 총 수입값
+            String preExpense = db.dao().getAmountOfDay(preYearMonth + " " + day1 + "일", "expense");      // 전월 해당 일 총 지출값
+            String preIncome  = db.dao().getAmountOfDay(preYearMonth + " " + day1 + "일", "income");       // 전월 해당 일 총 수입값
 
             if(preExpense != null) { preExpense = decFormat.format(Integer.parseInt(preExpense)); }         //null 일때 변환하면 팅김
             if(preIncome != null)  { preIncome  = decFormat.format(Integer.parseInt(preIncome)); }          //null 일때 변환하면 팅김
@@ -102,8 +102,8 @@ class CalendarAdapter extends RecyclerView.Adapter<CalendarAdapter.CalendarViewH
         }
 
         else if(daysOfMonth.get(position).contains("?")){   // 다음월 일 때~
-            String nextExpense = db.dao().getAmount(nextYearMonth + " " + day1 + "일", "expense");    // 다음월 해당 일 총 지출값
-            String nextIncome  = db.dao().getAmount(nextYearMonth + " " + day1 + "일", "income");     // 다음월 해당 일 총 수입값
+            String nextExpense = db.dao().getAmountOfDay(nextYearMonth + " " + day1 + "일", "expense");    // 다음월 해당 일 총 지출값
+            String nextIncome  = db.dao().getAmountOfDay(nextYearMonth + " " + day1 + "일", "income");     // 다음월 해당 일 총 수입값
 
             if(nextExpense != null) { nextExpense = decFormat.format(Integer.parseInt(nextExpense)); }      //null 일때 변환하면 팅김
             if(nextIncome != null)  { nextIncome  = decFormat.format(Integer.parseInt(nextIncome)); }       //null 일때 변환하면 팅김
@@ -126,8 +126,8 @@ class CalendarAdapter extends RecyclerView.Adapter<CalendarAdapter.CalendarViewH
         }
 
         else{                                                // 현재월 일 때~
-            String expense = db.dao().getAmount(nowYM + " " + day1 + "일", "expense");                      // 현재 해당 일 총 지출값
-            String income  = db.dao().getAmount(nowYM + " " + day1 + "일", "income");                       // 현재 해당 일 총 수입값
+            String expense = db.dao().getAmountOfDay(nowYM + " " + day1 + "일", "expense");                      // 현재 해당 일 총 지출값
+            String income  = db.dao().getAmountOfDay(nowYM + " " + day1 + "일", "income");                       // 현재 해당 일 총 수입값
 
             if(expense != null) { expense = decFormat.format(Integer.parseInt(expense)); }                  //null 일때 변환하면 팅김
             if(income != null)  { income  = decFormat.format(Integer.parseInt(income)); }                   //null 일때 변환하면 팅김
