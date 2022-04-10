8bdb817501e9392a0cd956be63c963f3cc4d5355
LEEhj <dlgkrwns24@naver.com>
2022-04-10 21:37:25 +0900

    워치기능 on/off시의 UI 업데이트

--- app/src/main/java/com/team_3/accountbook/WatchSettingActivity.java
+++ app/src/main/java/com/team_3/accountbook/WatchSettingActivity.java
@@ -13,10 +13,12 @@ import androidx.appcompat.app.AppCompatActivity;
 public class WatchSettingActivity extends AppCompatActivity {
     EditText amountGoal, warning;
     ImageView back;
-    TextView save;
+    TextView save, mGoal, mWarning, mWon, mPercent;
     Switch mSwitch;
     AppDatabase db;
     LinearLayout layout;
+
+
     @SuppressLint("NewApi")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +30,10 @@ public class WatchSettingActivity extends AppCompatActivity {
         back = findViewById(R.id.toBack);
         warning = findViewById(R.id.warning);
         save = findViewById(R.id.save);
+        mGoal = findViewById(R.id.tv_goal);
+        mWarning = findViewById(R.id.tv_warning);
+        mWon = findViewById(R.id.won);
+        mPercent = findViewById(R.id.tv_percent);
         layout = findViewById(R.id.layout);
 
         amountGoal.addTextChangedListener(new AddActivity.NumberTextWatcher(amountGoal));            // 금액 입력반응
@@ -56,15 +62,36 @@ public class WatchSettingActivity extends AppCompatActivity {
             finish();
         });
     }
+
+
+
     @SuppressLint("ResourceAsColor")
     void setEnable(){
         if (mSwitch.isChecked()){
             layout.setBackgroundColor(getResources().getColor(R.color.white));
+
+            mGoal.setTextColor(getResources().getColor(R.color.black));
+            amountGoal.setTextColor(getResources().getColor(R.color.hardDarkGray));
+            mWon.setTextColor(getResources().getColor(R.color.hardDarkGray));
+
+            mWarning.setTextColor(getResources().getColor(R.color.black));
+            warning.setTextColor(getResources().getColor(R.color.hardDarkGray));
+            mPercent.setTextColor(getResources().getColor(R.color.hardDarkGray));
+
             amountGoal.setEnabled(true);
             warning.setEnabled(true);
         }
         else {
-            layout.setBackgroundColor(getResources().getColor(R.color.lightGray));
+            layout.setBackgroundColor(getResources().getColor(R.color.hardLightGray));
+
+            mGoal.setTextColor(getResources().getColor(R.color.gray));
+            amountGoal.setTextColor(getResources().getColor(R.color.gray));
+            mWon.setTextColor(getResources().getColor(R.color.gray));
+
+            mWarning.setTextColor(getResources().getColor(R.color.gray));
+            warning.setTextColor(getResources().getColor(R.color.gray));
+            mPercent.setTextColor(getResources().getColor(R.color.gray));
+
             amountGoal.setEnabled(false);
             warning.setEnabled(false);
         }
